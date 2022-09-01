e5c51d4b943cbf53876a03932fcd6476f06572ed
maciej ślusarz <maciey24@o2.pl>
2022-09-01 20:31:10 +0200

    ex10

---
+++ learn-python-the-hard-way/ex10/ex10.py
@@ -0,0 +1,28 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+# while True:
+#  for i in ["/","- ","|","\\","|"]:
+#   print "%s\r" % i,
+
+print '''
+""""""
+'''
+
+print "imie\tnazwisko\n%s\t%s" % ("Maciej", "Slusarz")
+
+print "%r|%r" % ("abc \" def", "abc \' def")
+print "%s|%s" % ("abc \" def", "abc \' def")
