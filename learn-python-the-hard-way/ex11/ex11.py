5748ea14c4f103c080136c719942361b82b0b32b
maciej ślusarz <maciey24@o2.pl>
2022-09-05 01:06:19 +0200

    ex11

---
+++ learn-python-the-hard-way/ex11/ex11.py
@@ -0,0 +1,21 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# print "now, what's your name?"
+# name = raw_input()
+# print "Hello, %s, %r" % (name, name)
+#
+# print "What's your mother's name?"
+# mother_name = raw_input()
+# print "What's your father's name?"
+# father_name = raw_input()
+#
+# print "So, you are %r, the child of %s and %s." % (name, mother_name, father_name)
+
+print age + height
